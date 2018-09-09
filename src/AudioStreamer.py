62e1b3d24c9f692709debdd316d7c7ee9c1ea6a3
jalbatross <ih8u2o@gmail.com>
2018-09-09 02:29:41 -0700

    Naive tests passed
    - Successfully streamed audio from service

--- src/AudioStreamer.py
+++ src/AudioStreamer.py
@@ -1,4 +1,44 @@
+import json
+import requests
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+import io
+
 class AudioStreamer:
 
   def __init__(self):
     pass
+
+  def getFile(self, fileUrl):
+    return requests.get(fileUrl, verify=False)
+
+  def getTextStream(self, streamUrl):
+    r = requests.get(streamUrl, stream=True)
+
+    streamData = []
+    for line in r.iter_lines():
+      if line:
+        decoded_line = line.decode('utf-8')
+        streamData.append(decoded_line)
+
+    return streamData
+
+  def getAudioFileStream(self, streamUrl):
+    r = requests.get(streamUrl, stream=True)
+    maxChunks = 1000
+
+    chunks = 0
+    with open('somedata', 'wb') as fd:
+      for chunk in r.iter_content(chunk_size=128):
+        fd.write(chunk)
+        chunks += 1
+
+        if (chunks > maxChunks):
+          break
+
+    return fd
+
+
+
--- src/AudioStreamer.test.py
+++ src/AudioStreamer.test.py
@@ -8,15 +8,15 @@ class AudioStreamerTest(unittest.TestCase):
   
   # AudioStreamer is able to download a single regular file from a url
   def test_get_file(self):
-    self.assertIsNotNone(self.streamer.getFile('aUrl'))
+    self.assertIsNotNone(self.streamer.getFile('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'))
 
-  # AudioStreamer should be able to get data from a file stream from a url
-  def test_get_file_stream(self):
-    self.assertIsNotNone(self.streamer.getFileStream('streamUrl'))
+  # AudioStreamer should be able to get data from a text stream
+  def test_get_text_stream(self):
+    self.assertIsNotNone(self.streamer.getTextStream('http://httpbin.org/stream/20'))
 
   # AudioStreamer should get audio file streams from urls
   def test_get_audio_stream(self):
-    self.assertIsNotNone(self.streamer.getAudioFileStream('audioStreamUrl'))
+    self.assertIsNotNone(self.streamer.getAudioFileStream(audioStreamUrl))
 
 if __name__ == '__main__':
     unittest.main()
