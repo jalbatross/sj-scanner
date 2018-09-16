553bd7836533b1ebf5cb1daddfbfad36c3d3bf29
jalbatross <ih8u2o@gmail.com>
2018-09-15 19:43:36 -0700

    Faster threshold decay, auto save to wav
    - Exponential threshold decay. Doubles in reduction for no sound received in stream if we have at least 5kb in the data buffer
    - Auto encodes processed audio data to wav
    - Added function for Google cloud speech recognition, still unused

--- src/AudioStreamer.py
+++ src/AudioStreamer.py
@@ -3,10 +3,21 @@ import requests
 import threading
 import time
 import sys
+import io
 
 from pydub import AudioSegment
 from datetime import datetime
 from pathlib import Path
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+from ffmpy import FFmpeg
+
+import os
+
+
 
 class AudioStreamer:
 
@@ -35,20 +46,33 @@ class AudioStreamer:
     threshold = 0
     dataBuffer = []
 
+    prevDelta = 0
+    delta = 0
     for chunk in r.iter_content(chunk_size=512):
       
-      threshold = threshold + self.processAudio(chunk)
+      delta = self.processAudio(chunk)
+      
+      if (prevDelta < 0 and delta < 0 and len(dataBuffer) > 20):
+        delta = prevDelta * 2
+
+      prevDelta = delta
+
+      threshold = threshold + delta
 
       if (threshold <= 0):
         threshold = 0
+
         if (len(dataBuffer) <= 0):
-          continue
+          pass
+        elif (len(dataBuffer) >1 and len(dataBuffer) <= 10) :
+          dataBuffer = []
         else:
           copiedBuf = dataBuffer[:]
           dataBuffer = []
           t = threading.Thread(target=self.buildAudioFile, args=(copiedBuf,))
           t.start()
-          continue
+
+        continue
 
       # otherwise there was sound
       print(chunks, ' thresh: ', threshold)
@@ -62,6 +86,7 @@ class AudioStreamer:
     return 0
 
   def processAudio(self, data):
+
     temp = open('temp', 'wb')
     temp.write(data)
     temp.close()
@@ -70,9 +95,9 @@ class AudioStreamer:
       audio = AudioSegment.from_mp3('temp')
 
       if (audio.max >= 1000):
-        return 10
+        return 15
       else:
-        return -10
+        return -2
 
     except:
       return 0
@@ -84,11 +109,24 @@ class AudioStreamer:
   #
   def buildAudioFile(self, buf):
     fileTitle = str(datetime.now())
-    path = Path('audioLogs/%s.mp3' % fileTitle)
+
+    pathMp3 = Path('audioLogs/%s.mp3' % fileTitle)
+    
+    
     try:
-      with open(path, 'wb') as fd:
+      with open(pathMp3, 'wb') as fd:
         for data in buf:
           fd.write(data)
+      fd.close()
+
+      pathWav = Path('audioLogs/%s.wav' % fileTitle)
+
+      ff = FFmpeg(
+        inputs={str(pathMp3): None},
+        outputs={str(pathWav): '-acodec pcm_s16le -ac 1 -ar 16000'}
+      )
+      ff.run()
+
     except:
       print("Unexpected error:", sys.exc_info()[0])
       return False
@@ -98,6 +136,31 @@ class AudioStreamer:
 
 
 
+
+
+
+def transcribe_file(speech_file):
+    """Transcribe the given audio file."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    with io.open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US')
+
+    response = client.recognize(config, audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
 
 
 
